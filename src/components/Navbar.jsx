727209ba378a3f655f1cb3a54ea37bc4d97c8bf3
Bidhan Baral <bbaral2062@gmail.com>
2025-07-13 10:24:13 -0400

    Navbar and Footer complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar bg-base-100 shadow-sm px-6 py-3">
+      <div className="navbar-start">
+        <Link to="/" className="btn btn-ghost text-xl font-bold text-primary flex items-center gap-3">
+          <img src={logo} alt="CraftFolio Logo" className="w-12 h-12 object-contain" />
+          CraftFolio
+        </Link>
+      </div>
+    </div>
   )
 }
 
